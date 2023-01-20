750c7e79ee2d6c175b73d77bf87ad9b0876eb279
Gustavo Miranda <oachgustavo@gmail.com>
2023-01-20 19:26:48 -0300

    https://spring.io/guides/gs/scheduling-tasks/ - Scheduling Tasks

--- src/main/java/com/github/gustavovitor/relearning_spring_framework/Value.java
+++
@@ -1,7 +0,0 @@
-package com.github.gustavovitor.relearning_spring_framework;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record Value(Long id, String quote) {
-}
