56c4262aa24755f0708faddaa4871bd216987bc9
Gustavo Miranda <oachgustavo@gmail.com>
2023-01-20 00:11:18 -0300

    https://spring.io/guides/gs/rest-service/ - Building a RESTful Web Service

---
+++ src/main/java/com/github/gustavovitor/relearning_spring_framework/Greeting.java
@@ -0,0 +1,3 @@
+package com.github.gustavovitor.relearning_spring_framework;
+
+public record Greeting(long id, String content) { }
---
+++ src/test/java/com/github/gustavovitor/relearning_spring_framework/RelearningSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gustavovitor.relearning_spring_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelearningSpringFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
